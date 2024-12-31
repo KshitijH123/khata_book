a164b5f9c879358592339364078b48ae157aedce
KshitijH123 <kshitijhapase@gmail.com>
2024-12-31 23:46:47 +0530

    changes

---
+++ lib/splash_screen.dart
@@ -0,0 +1,44 @@
+import 'dart:async';
+
+import 'package:flutter/material.dart';
+import 'package:khata_book/home_page.dart';
+
+class SplashScreen extends StatefulWidget {
+  const SplashScreen({super.key});
+
+  @override
+  State<SplashScreen> createState() => _SplashScreenState();
+}
+
+class _SplashScreenState  extends State<SplashScreen> {
+  @override
+  void initState() {
+    super.initState();
+    Timer(const Duration(seconds: 10), () {
+      Navigator.of(context).pushReplacement(
+            MaterialPageRoute(builder: (_) => const MyHomePage(title: '',)),
+          );
+    });
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+     body: Container(
+      decoration: const BoxDecoration(
+        gradient: LinearGradient(colors: [
+          Color.fromARGB(255, 136, 167, 220),
+          Color.fromARGB(255, 122, 192, 224),
+          Color.fromARGB(255, 175, 224, 247)
+        ],
+        begin: Alignment.topLeft,
+       end: Alignment.bottomRight,
+        ),
+
+      ),
+      child: Center(
+       child: Image.asset('asset/images/blue.png',height: 200,width: 200,) ),
+     ),
+    );
+  }
+}
