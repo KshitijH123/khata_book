a41d54d3bb88595470c1ad5ab9a3a1d87d074dd5
KshitijH123 <kshitijhapase@gmail.com>
2025-01-03 23:18:14 +0530

    search bar

--- lib/customer_details.dart
+++ lib/customer_details.dart
@@ -1,7 +1,20 @@
 import 'package:flutter/material.dart';
 
-class CustomerDetail extends StatelessWidget {
+class CustomerDetail extends StatefulWidget {
   const CustomerDetail({super.key});
+
+  @override
+  _CustomerDetailState createState() => _CustomerDetailState();
+}
+
+class _CustomerDetailState extends State<CustomerDetail> {
+  final TextEditingController _searchController = TextEditingController();
+
+  @override
+  void dispose() {
+    _searchController.dispose();
+    super.dispose();
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -9,8 +22,23 @@ class CustomerDetail extends StatelessWidget {
       appBar: AppBar(
         title: const Text('Customer Detail'),
       ),
-      body: const Center(
-        child: Text('Enter customer details here.'),
+      body: Padding(
+        padding: const EdgeInsets.all(16.0),
+        child: Column(
+          children: [
+            // Search bar with text and icon
+            TextField(
+              controller: _searchController,
+              decoration: InputDecoration(
+                labelText: 'Search',
+                prefixIcon: const Icon(Icons.search),
+                border: OutlineInputBorder(
+                  borderRadius: BorderRadius.circular(10.0),
+                ),
+              ),
+            ),
+          ],
+        ),
       ),
     );
   }
