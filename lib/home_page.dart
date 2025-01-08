5b974e474f6ea654e6281181632206c19407998d
KshitijH123 <kshitijhapase@gmail.com>
2025-01-08 22:48:09 +0530

    bottom navigation bar

--- lib/home_page.dart
+++ lib/home_page.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:khata_book/customer_details.dart';
+import 'package:curved_navigation_bar/curved_navigation_bar.dart';
 
 class MyHomePage extends StatefulWidget {
   const MyHomePage({super.key, required this.title});
@@ -14,6 +15,7 @@ class _MyHomePageState extends State<MyHomePage> {
   List<Map<String, dynamic>> transactions = [];
   List<Map<String, dynamic>> filteredTransactions = [];
   final TextEditingController _searchController = TextEditingController();
+  int _selectedIndex = 0;
 
   @override
   void initState() {
@@ -67,7 +69,8 @@ class _MyHomePageState extends State<MyHomePage> {
               });
               Navigator.of(context).pop();
             },
-            child: const Text('Delete', style: TextStyle(color: Color.fromARGB(255, 243, 101, 101))),
+            child: const Text('Delete',
+                style: TextStyle(color: Color.fromARGB(255, 243, 101, 101))),
           ),
         ],
       ),
@@ -90,6 +93,95 @@ class _MyHomePageState extends State<MyHomePage> {
     }
   }
 
+  void _onItemTapped(int index) {
+    setState(() {
+      _selectedIndex = index;
+    });
+  }
+
+  Widget _getPage() {
+    switch (_selectedIndex) {
+      case 0:
+        return Padding(
+          padding: const EdgeInsets.all(16.0),
+          child: Column(
+            children: [
+              Padding(
+                padding: const EdgeInsets.symmetric(vertical: 8.0),
+                child: TextField(
+                  controller: _searchController,
+                  decoration: InputDecoration(
+                    labelText: 'Search',
+                    border: OutlineInputBorder(
+                      borderRadius: BorderRadius.circular(20.0),
+                    ),
+                    prefixIcon: const Icon(Icons.search_outlined),
+                  ),
+                ),
+              ),
+              Expanded(
+                child: ListView.builder(
+                  itemCount: filteredTransactions.length,
+                  itemBuilder: (context, index) {
+                    final transaction = filteredTransactions[index];
+                    return Card(
+                      margin: const EdgeInsets.only(bottom: 16),
+                      elevation: 5,
+                      shape: RoundedRectangleBorder(
+                        borderRadius: BorderRadius.circular(10.0),
+                      ),
+                      child: GestureDetector(
+                        onTap: () => navigateToEditTransaction(index),
+                        child: Padding(
+                          padding: const EdgeInsets.symmetric(
+                            vertical: 8.0,
+                            horizontal: 12.0,
+                          ),
+                          child: ListTile(
+                            contentPadding: EdgeInsets.zero,
+                            title: Row(
+                              children: [
+                                Expanded(
+                                  child: Text(
+                                    transaction['name'],
+                                    style: const TextStyle(fontSize: 20),
+                                  ),
+                                ),
+                                Text(
+                                  '\₹${transaction['amount']}',
+                                  style: TextStyle(
+                                    color: transaction['isMoneyGiven']
+                                        ? Colors.green
+                                        : Colors.red,
+                                    fontSize: 20,
+                                  ),
+                                ),
+                              ],
+                            ),
+                            trailing: IconButton(
+                              icon: const Icon(Icons.delete,
+                                  color: Color.fromARGB(255, 237, 103, 93)),
+                              onPressed: () => deleteTransaction(index),
+                            ),
+                          ),
+                        ),
+                      ),
+                    );
+                  },
+                ),
+              ),
+            ],
+          ),
+        );
+      case 1:
+        return Center(child: Text('Search Page'));
+      case 2:
+        return Center(child: Text('App Info'));
+      default:
+        return Container();
+    }
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
@@ -97,91 +189,37 @@ class _MyHomePageState extends State<MyHomePage> {
         backgroundColor: Theme.of(context).colorScheme.inversePrimary,
         title: const Text('Khata Book', style: TextStyle(fontSize: 26)),
       ),
-      body: Padding(
-        padding: const EdgeInsets.all(16.0),
-        child: Column(
-          children: [
-            Padding(
-              padding: const EdgeInsets.symmetric(vertical: 8.0),
-              child: TextField(
-                controller: _searchController,
-                decoration: InputDecoration(
-                  labelText: 'Search',
-                  border: OutlineInputBorder(
-                    borderRadius: BorderRadius.circular(20.0),
+      body: _getPage(),
+      floatingActionButton: _selectedIndex == 0
+          ? FloatingActionButton.extended(
+              onPressed: () async {
+                final newTransaction = await Navigator.push(
+                  context,
+                  MaterialPageRoute(
+                    builder: (context) => const CustomerDetail(),
                   ),
-                  prefixIcon: const Icon(Icons.search_outlined),
-                ),
-              ),
-            ),
-            Expanded(
-              child: ListView.builder(
-                itemCount: filteredTransactions.length,
-                itemBuilder: (context, index) {
-                  final transaction = filteredTransactions[index];
-                  return Card(
-                    margin: const EdgeInsets.only(bottom: 16),
-                    elevation: 5,
-                    shape: RoundedRectangleBorder(
-                      borderRadius: BorderRadius.circular(10.0),
-                    ),
-                    child: GestureDetector(
-                      onTap: () =>
-                          navigateToEditTransaction(index), 
-                      child: Padding(
-                        padding: const EdgeInsets.symmetric(
-                          vertical: 8.0,
-                          horizontal: 12.0,
-                        ),
-                        child: ListTile(
-                          contentPadding: EdgeInsets.zero,
-                          title: Row(
-                            children: [
-                              Expanded(
-                                child: Text(
-                                  transaction['name'],
-                                  style: const TextStyle(fontSize: 20),
-                                ),
-                              ),
-                              Text(
-                                '\₹${transaction['amount']}',
-                                style: TextStyle(
-                                  color: transaction['isMoneyGiven']
-                                      ? Colors.green
-                                      : Colors.red,
-                                  fontSize: 20,
-                                ),
-                              ),
-                            ],
-                          ),
-                          trailing: IconButton(
-                            icon: const Icon(Icons.delete, color: Color.fromARGB(255, 237, 103, 93)),
-                            onPressed: () => deleteTransaction(index),
-                          ),
-                        ),
-                      ),
-                    ),
-                  );
-                },
-              ),
-            ),
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton.extended(
-        onPressed: () async {
-          final newTransaction = await Navigator.push(
-            context,
-            MaterialPageRoute(
-              builder: (context) => const CustomerDetail(),
-            ),
-          );
-          if (newTransaction != null) {
-            addTransaction(newTransaction);
-          }
-        },
-        label: const Text('Add Customer'),
-        icon: const Icon(Icons.person_add),
+                );
+                if (newTransaction != null) {
+                  addTransaction(newTransaction);
+                }
+              },
+              label: const Text('Add Customer'),
+              icon: const Icon(Icons.person_add),
+            )
+          : null,
+      bottomNavigationBar: CurvedNavigationBar(
+        backgroundColor: Colors.transparent,
+        color: Color.fromARGB(255, 212, 192, 243), 
+        height: 55.0,
+        animationDuration:
+            const Duration(milliseconds: 300), 
+        onTap: _onItemTapped,
+        items: const <Widget>[
+          Icon(Icons.home_outlined, size: 30),
+          Icon(Icons.search_sharp, size: 30),
+          Icon(Icons.info_outlined, size: 30),
+          
+        ],
       ),
     );
   }
