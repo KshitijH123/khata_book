7288436e882e7831a4babfe6f1ff57c7aa60dc08
KshitijH123 <kshitijhapase@gmail.com>
2025-01-16 23:17:30 +0530

    splash screen colour

--- lib/main.dart
+++ lib/main.dart
@@ -13,11 +13,10 @@ class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return GetMaterialApp(
-      title: 'Flutter Demo',
       theme: ThemeData(
         colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
         useMaterial3: true,
-      ),
+      ),  
       home: const SplashScreen(),
       debugShowCheckedModeBanner: false,
     );
--- lib/screens/splash_screen.dart
+++ lib/screens/splash_screen.dart
@@ -26,17 +26,7 @@ class _SplashScreenState  extends State<SplashScreen> {
   Widget build(BuildContext context) {
     return Scaffold(
      body: Container(
-      decoration: const BoxDecoration(
-        gradient: LinearGradient(colors: [
-          Color.fromARGB(255, 136, 167, 220),
-          Color.fromARGB(255, 122, 192, 224),
-          Color.fromARGB(255, 175, 224, 247)
-        ],
-        begin: Alignment.topLeft,
-       end: Alignment.bottomRight,
-        ),
-
-      ),
+     color: Colors.white,
       child: Center(
        child: Image.asset('asset/images/blue.png',height: 200,width: 200,) ),
      ),
