15a63b160dd62c3e990d25f0017d404d7a212c80
KshitijH123 <kshitijhapase@gmail.com>
2025-01-17 23:00:14 +0530

    created a ledger for total

--- lib/screens/home/home_page_screen.dart
+++ lib/screens/home/home_page_screen.dart
@@ -2,6 +2,7 @@ import 'package:curved_navigation_bar/curved_navigation_bar.dart';
 import 'package:flutter/material.dart';
 import 'package:get/get.dart';
 import 'package:khata_book/screens/add/transactions/add_transaction_details_screen.dart';
+import 'package:khata_book/screens/home/controller/transaction_list_controller.dart';
 import 'package:khata_book/screens/home/search_page.dart';
 import 'package:khata_book/screens/home/widget/transaction_list.dart';
 
@@ -16,6 +17,15 @@ class _HomePageScreenState extends State<HomePageScreen> {
   int selectedIndex = 0;
   VoidCallback? refreshTransactionList;
 
+  final TransactionListController controller =
+      Get.put(TransactionListController());
+
+  @override
+  void initState() {
+    super.initState();
+    controller.fetchAllTransactions();
+  }
+
   Widget pageContent() {
     switch (selectedIndex) {
       case 0:
@@ -29,7 +39,8 @@ class _HomePageScreenState extends State<HomePageScreen> {
           reserveCallback: (refreshTransactionList) {
             this.refreshTransactionList = refreshTransactionList;
           },
-        );      case 2:
+        );
+      case 2:
         return const Center(child: Text('App Info'));
       default:
         return Container();
@@ -38,12 +49,47 @@ class _HomePageScreenState extends State<HomePageScreen> {
 
   @override
   Widget build(BuildContext context) {
+    final totalMoneyGiven = controller.totalMoneyGiven;
+    final totalMoneyToGive = controller.totalMoneyToGive;
+
     return Scaffold(
       appBar: AppBar(
         backgroundColor: Theme.of(context).colorScheme.inversePrimary,
         title: const Text('Khata Book', style: TextStyle(fontSize: 26)),
       ),
-      body: pageContent(),
+      body: Column(
+        children: [
+          Padding(
+            padding: const EdgeInsets.all(8.0),
+            child: Row(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
+                  children: [
+                    const Text('Money Given', style: TextStyle(fontSize: 16)),
+                    Text(
+                      '₹$totalMoneyGiven',
+                      style: const TextStyle(fontSize: 20, color: Colors.green),
+                    ),
+                  ],
+                ),
+                Column(
+                  crossAxisAlignment: CrossAxisAlignment.end,
+                  children: [
+                    const Text('Money To Give', style: TextStyle(fontSize: 16)),
+                    Text(
+                      '₹$totalMoneyToGive',
+                      style: const TextStyle(fontSize: 20, color: Colors.red),
+                    ),
+                  ],
+                ),
+              ],
+            ),
+          ),
+          Expanded(child: pageContent()),
+        ],
+      ),
       floatingActionButton: selectedIndex == 0
           ? FloatingActionButton.extended(
               onPressed: () async {
